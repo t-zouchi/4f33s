b5e64e71167184873d597f2c3f4c5194002d34f2
t-zouchi <mojamoja.000@gmail.com>
2016-10-30 23:23:16 +0900

    create platform

---
+++ Assets/Scripts/Scene.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Scene : MonoBehaviour
+{
+  public void start()
+  {
+    SceneManager.LoadScene("Main");
+  }
+
+  public void introduction()
+  {
+    SceneManager.LoadScene("Introduction");
+  }
+
+  public void Title()
+  {
+    SceneManager.LoadScene("Title");
+  }
+
+  public void End()
+  {
+    SceneManager.LoadScene("End");
+  }
+
+  public void Tweet()
+  {
+    string message = "4分33秒を演奏しました (短縮URL) #nikonamakudou";
+    Application.OpenURL("http://twitter.com/intent/tweet?text=" + WWW.EscapeURL(message));
+  }
+}
